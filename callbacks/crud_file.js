78b292eba50540279ee3a329ec9e991f0c7cf4df
Yesus0212 <jesussolispadron@gmail.com>
2021-11-12 17:43:15 -0600

    Ejercicio CRUD FILE (file system)

---
+++ callbacks/crud_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function createFile (nameFile, contentFile){
+    fs.writeFile(nameFile, contentFile,'utf8', (error) => {
+        if(error) 
+            console.error(error);
+        else 
+            console.info(`El archivo ${nameFile}, se creo correctamente`);    
+    });
+}
+
+function deleteFile (nameFile){
+    fs.unlink(nameFile, (error) => {
+        if(error) console.error(error);
+        else console.info(`El archivo ${nameFile}, se elimino correctamente`);
+    })
+}
+
+
+function editFile (nameFile, contentUpdate){
+    fs.appendFile(nameFile, contentUpdate, 'utf8', (error) => {
+        if(error) console.error(error);
+        else console.info(`El archivo ${nameFile}, se actualizó correctamente`)
+    });
+};
+
+
+function readFile (nameFile){
+    fs.readFile(nameFile, 'utf8', function(error, data){
+        if(error) console.error(error);
+        else console.log(`El contenido del archivo ${nameFile} es: ${data}`);
+    });
+};
+
+createFile("Hola.txt", "Archivo nuevo");
