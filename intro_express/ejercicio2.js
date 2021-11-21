3afcd26501df717ea12d43cdb058c140e930d77a
Yesus0212 <jesussolispadron@gmail.com>
2021-11-20 22:19:49 -0600

    Ejercicio de insercion de nuevo registro en el json, recibiendo un json desde la peticion, terminado

--- intro_express/ejercicio2.js
+++ intro_express/ejercicio2.js
@@ -2,16 +2,16 @@ const express = require('express');
 const fs = require('fs/promises');
 
 const server = express(); // tambien le llaman server
+server.use(express.json());
 const port = 8080;
 
-// definimos un handler
-// para la ruta localhost:8080/ para el metodo GET
+const file = 'koders.json';
+
+// Leemos el archivo koders.json, se convierte a un objeto JSON para despues enviarlo como respuesta
 server.get('/koders', async (req, res) => {
-
     let result;
-
     try {
-        result = await fs.readFile('koders.json', 'utf-8');           
+        result = await fs.readFile(file, 'utf-8');           
     }
     catch(error){
         console.error(error);
@@ -21,6 +21,44 @@ server.get('/koders', async (req, res) => {
     res.json(response);
 });
 
+// Recibimos un objeto JSON, para insertarlo como elemento nuevo dentro del archivo de koders.json
+server.post('/koders', async (req, res) => {
+
+    const newElement = req.body;
+
+    const result = {
+        code: "",
+        message: ""
+    };
+
+    let info;
+    try {
+        info = await fs.readFile(file, 'utf-8');
+    }
+    catch(error){
+        console.error(error);
+        result.code = "01";
+        result.message = `No es posible encontrar el archivo ${file}`;
+    }
+    
+    const objInfo = JSON.parse(info);
+    objInfo.koders.push(newElement);
+    
+    const koders = JSON.stringify(objInfo);
+    
+    try {
+        info = await fs.writeFile(file, koders, 'utf-8');
+        result.code = "00";
+        result.message = "Insercion existosa";    
+    }
+    catch(error){
+        console.error(error);
+        result.code = "02";
+        result.message = "Error al crear el registro nuevo";
+    }
+
+    res.send(result);
+});
 
 // Aquí se inicia el server o app
 server.listen(port, () => {
