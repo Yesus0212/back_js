78e894b376bc991168c315f04d1d52784cf02deb
Yesus0212 <jesussolispadron@gmail.com>
2021-11-20 22:28:51 -0600

    Agregue algunos comentarios, para hacer mas legible el codigo

--- intro_express/ejercicio2.js
+++ intro_express/ejercicio2.js
@@ -2,10 +2,10 @@ const express = require('express');
 const fs = require('fs/promises');
 
 const server = express(); // tambien le llaman server
-server.use(express.json());
+server.use(express.json()); // Sirve para poder leer en el Body del request, el json que se esta enviando
 const port = 8080;
 
-const file = 'koders.json';
+const file = 'koders.json';  // constante con el nombre del archivo
 
 // Leemos el archivo koders.json, se convierte a un objeto JSON para despues enviarlo como respuesta
 server.get('/koders', async (req, res) => {
@@ -24,7 +24,7 @@ server.get('/koders', async (req, res) => {
 // Recibimos un objeto JSON, para insertarlo como elemento nuevo dentro del archivo de koders.json
 server.post('/koders', async (req, res) => {
 
-    const newElement = req.body;
+    const newElement = req.body;  // Aquí se recupera del request el objeto JSON que se esta enviando en el body y lo almaceno en la constante newElement
 
     const result = {
         code: "",
@@ -41,11 +41,12 @@ server.post('/koders', async (req, res) => {
         result.message = `No es posible encontrar el archivo ${file}`;
     }
     
-    const objInfo = JSON.parse(info);
-    objInfo.koders.push(newElement);
+    const objInfo = JSON.parse(info); // Parseo la información obtenida del archivo .json
+    objInfo.koders.push(newElement);  // Le agrego el nuevo elemento
     
-    const koders = JSON.stringify(objInfo);
+    const koders = JSON.stringify(objInfo);  // Vuelvo a convertir el objeto, para poder almacenarlo en el archivo, ya con el nuevo elemento agregado
     
+    // Reescribo el archivo, con el nuevo contenido.
     try {
         info = await fs.writeFile(file, koders, 'utf-8');
         result.code = "00";
