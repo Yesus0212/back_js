ecf8253753a60823d3f805f7477f7a33de79d1da
Yesus0212 <jesussolispadron@gmail.com>
2021-11-24 22:28:18 -0600

    Ejericio con router

---
+++ koders_api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routerKoders = require('./routers/koders')
+
+const port = 8080;
+
+const app = express();
+
+app.use(express.json()); 
+
+// Con esta línea manejamos las rutas definidas desde el archivo koders.js
+app.use('/koders', routerKoders);
+
+// Aquí se inicia el server o app
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ koders_api/routers/koders.js
@@ -0,0 +1,267 @@
+const express = require('express');
+const fs = require('fs/promises');
+
+const file = 'koders.json'; 
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    let koders;
+
+    const result = {
+        code: "",
+        message: "",
+    };
+
+    koders = await getKoders();
+
+    if(koders.length != 0){
+        result.code = "00";
+        result.message = `Operacion exitosa`
+    }
+    else{
+        result.code = "01";
+        result.message = `No fue posible encontrar el archivo ${file}`
+    }
+   
+    res.json({...result, ...koders});
+});
+
+
+router.post('/', async (req, res) => {
+    const koder = req.body;
+    let koders;
+
+    const result = {
+        code: "",
+        message: ""
+    };
+
+    koders = await getKoders();
+
+    if(koders.length == 0){
+        result.code = "01";
+        result.message = `No fue posible encontrar el archivo ${file}`
+
+        res.send(result);
+        return;
+    }
+
+    if(!isValidKoder(koder)){
+        result.code = "02";
+        result.message = `Datos invalidos`
+
+        res.send(result);
+        return;
+    }
+
+    koders.koders.push(koder);
+    
+    const validSave = await saveKoders(koders);
+
+    if(validSave){
+        res.statusCode = 201;
+        result.code = "00";
+        result.message = "Insercion existosa"; 
+    }
+    else{        
+        result.code = "03";
+        result.message = "Error al crear el registro nuevo";
+    }
+
+    res.send({...result, koder});
+});
+
+
+router.patch('/:id', async (req, res) => {
+    const koderId = req.params.id;    
+    const koder = req.body;
+    let koders;
+
+    const result = {
+        code: "",
+        message: ""
+    };
+    
+    koders = await getKoders();
+
+    if(koders.length == 0){
+        result.code = "01";
+        result.message = `No fue posible encontrar el archivo ${file}`
+
+        res.send(result);
+        return;
+    }
+
+    if(!isValidKoder(koder)){
+        result.code = "02";
+        result.message = `Datos invalidos`
+
+        res.send(result);
+        return;
+    }
+    
+    const koderIndex = koders.koders.findIndex((koder) => koder.nombre === koderId);
+
+    if(koderIndex === -1) {
+        result.code = "03";
+        result.message = "El koder no se encuentra en el archivo"
+
+        res.send(result);
+
+        return;
+    }
+
+    koders.koders[koderIndex].nombre = koder.nombre;
+    koders.koders[koderIndex].genero = koder.genero;
+
+    const validSave = await saveKoders(koders);
+
+    if(validSave){
+        result.code = "00";
+        result.message = "Actualizacion existosa"; 
+    }
+    else{        
+        result.code = "04";
+        result.message = "Error al actualizar el registro nuevo";
+    }
+
+    res.send({...result, ...koders});
+});
+
+
+router.get('/:id', async (req, res) => {
+    const koderId = req.params.id;
+
+    console.log(koderId);
+
+    let result = {
+        code: "",
+        message: "",
+        koder: ""
+    };
+
+    let initData;
+    try {
+        initData = await fs.readFile(file, 'utf-8');
+    }
+    catch(error){
+        console.error(error);
+        result.code = "01";
+        result.message = `No es posible encontrar el archivo ${file}`;
+    }
+    
+    try {
+        const objInfo = JSON.parse(initData); 
+        const filterKoder = objInfo.koders.filter(koder => {
+            return koder.nombre === koderId;
+        });
+
+        if(filterKoder.length){
+            result.code = "00";
+            result.message = "Busqueda exitosa";
+    
+            result.koder = filterKoder;
+        
+            const koder = JSON.stringify(result, null, 4); 
+        }        
+        else{
+            result.code = "03";
+            result.message = `El koder ${koderId} no se encuentra en el archivo`;
+        }
+    }
+    catch(error){
+        result.code = "02";
+        result.message = `Error al buscar al koder ${koderId}`;
+    }
+    
+    res.send(result);
+});
+
+router.delete('/:id', async (req, res) => {
+    const koderId = req.params.id;
+
+    console.log(koderId);
+
+    let result = {
+        code: "",
+        message: "",
+    };
+
+    let initData;
+    try {
+        initData = await fs.readFile(file, 'utf-8');
+    }
+    catch(error){
+        console.error(error);
+        result.code = "01";
+        result.message = `No es posible encontrar el archivo ${file}`;
+    }
+    
+    try {
+        const objInfo = JSON.parse(initData); 
+
+        const filterKoder = {
+            koders : ""
+        };
+        
+        filterKoder.koders = objInfo.koders.filter(koder => { 
+            return koder.nombre !== koderId; 
+        });
+
+        if(filterKoder.koders.length){
+            result.code = "00";
+            result.message = "Eliminacion exitosa";
+
+            const koders = JSON.stringify(filterKoder, null, 4);
+    
+            initData = await fs.writeFile(file, koders, 'utf-8');
+        }        
+        else{
+            result.code = "03";
+            result.message = `El koder ${koderId} no se encuentra en el archivo`;
+        }
+    }
+    catch(error){
+        result.code = "02";
+        result.message = `Error al eliminar al koder ${koderId}`;
+    }
+    
+    res.send(result);
+});
+
+function isValidKoder(koder) {
+    return koder.nombre && koder.genero
+}
+
+async function getKoders(){
+
+    let dataParse;
+
+    try {
+        const data = await fs.readFile(file, 'utf-8');
+        if(data != null)
+            dataParse = JSON.parse(data);
+
+        return dataParse; 
+    }
+    catch(error){
+        return [];
+    }    
+}
+
+async function saveKoders(dataKoders){
+      
+      try {          
+          const koders = JSON.stringify(dataKoders, null, 4);
+          await fs.writeFile(file, koders, 'utf-8');
+          
+          return true;
+      }
+      catch(error){
+          return false;
+      }
+}
+
+// Por convención, el exports debe ir al final del archivo
+module.exports = router;
